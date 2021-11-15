e633ec73a581978ca81d736b296288b4397959cf
rijhwaniritesh24 <84992462+rijhwaniritesh24@users.noreply.github.com>
2021-11-15 23:34:33 +0530

    Add files via upload

---
+++ webjs.js
@@ -0,0 +1,16 @@
+function triger(){
+    console.log(document.documentElement.scrollTop);
+    let cl=document.querySelector(".nav");
+    if(document.documentElement.scrollTop> 50){
+        
+        cl.classList.add("nav_sticky");
+       
+        
+    }
+    else{
+        cl.classList.remove("nav_sticky");
+        // cl.classList.add("nav");
+    }
+}
+
+document.body.onscroll= triger;
